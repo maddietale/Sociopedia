99f524db92acb1ed8907f2070e182b5d193ac6cd
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2023-05-20 13:17:04 +0330

    Setup Navbar

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import authReducer from "./state";
+import 'index.css';
+import App from 'App';
+import authReducer from "state";
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
