ac40d9ca52aa629da0577292cb7af4e860a46880
Mahdieh Torabi <mahdiehtorabi00@gmail.com>
2023-05-24 00:53:12 +0330

    Complete project

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { login } from "../controllers/auth.js";
-// import User from "../models/User.js";
+import User from "../models/User.js";
 
 const router = express.Router();
 
-// router.get("/users", async (req, res) => {
-//     const user = await User.find();
-//     res.json(user);
-// });
+router.get("/users", async (req, res) => {
+    const user = await User.find();
+    res.json(user);
+});
 router.post("/login", login);
 
 export default router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -13,6 +13,6 @@ router.get("/:id", verifyToken, getUser);
 router.get("/:id/friends", verifyToken, getUserFriends);
 
 /* UPDATE */
-router.patch("/:id/friendId", verifyToken, addRemoveFriends);
+router.patch("/:id/:friendId", verifyToken, addRemoveFriends);
 
 export default router;
